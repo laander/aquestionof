7e06265fec2ff92f07edb8292150af769bf9031f
laander <la@laander.com>
2011-04-13 19:36:43 +0200

    made 404 pages (non existing) redirect to default start page

--- wp-content/themes/custom/404.php
+++ wp-content/themes/custom/404.php
@@ -5,18 +5,7 @@
  * The 404 template is used when a reader visits an invalid URL on your site.
  */
 
-@header( 'HTTP/1.1 404 Not found', true, 404 );
+header( 'HTTP/1.1 404 Not found', false, 404 );
+header( 'Location: ' .  get_bloginfo( 'wpurl' ) ) ;
 
-get_header(); ?>
-
-	<div id="content" class="hfeed content">
-
-			<div id="post-0" class="<?php hybrid_entry_class(); ?>">
-				
-				<span>We couldn't find the page you're looking for. Seems like the greedy page-eating monkeys paid us a visit.</span>
-
-			</div><!-- .hentry -->
-
-	</div><!-- .content .hfeed -->
-
-<?php get_footer(); ?>
+?>
